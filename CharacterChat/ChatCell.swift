15a5fc8d8e88ab04b193061139391568783deea0
Francesco Agosti <francesco.agosti93@gmail.com>
2017-08-30 18:07:48 -0700

    Basic version of avatar working

--- CharacterChat/ChatCell.swift
+++ CharacterChat/ChatCell.swift
@@ -12,11 +12,18 @@ final class ChatCell: UITableViewCell {
     
     var chatBubbleView: ChatBubbleView?
     let senderLabel: UILabel = UILabel()
- 
+    let senderAvatar: UIImageView = {
+        let avatar = UIImage(named: "avatar")
+        let imageView = UIImageView(image: avatar)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .none
         contentView.addSubview(senderLabel)
+        contentView.addSubview(senderAvatar)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,12 +37,16 @@ private extension ChatCell {
         guard let chatBubbleView = chatBubbleView else { return }
         chatBubbleView.translatesAutoresizingMaskIntoConstraints = false
         senderLabel.translatesAutoresizingMaskIntoConstraints = false
+        senderAvatar.translatesAutoresizingMaskIntoConstraints = false
+        
         let views = [
             "chatBubbleView": chatBubbleView,
             "senderLabel": senderLabel,
+            "senderAvatar": senderAvatar,
         ]
         var constraints = [
             NSLayoutConstraint.constraints(withVisualFormat: "V:|[senderLabel][chatBubbleView]|", options: [], metrics: nil, views: views),
+            NSLayoutConstraint.constraints(withVisualFormat: "V:[senderAvatar(25)]|", options: [], metrics: nil, views: views),
         ]
         switch(sender) {
         case .sent:
@@ -43,7 +54,7 @@ private extension ChatCell {
             constraints.append(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(64)-[chatBubbleView]|", options: [], metrics: nil, views: views))
         case .received:
             constraints.append(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(32)-[senderLabel]", options: [], metrics: nil, views: views))
-            constraints.append(NSLayoutConstraint.constraints(withVisualFormat: "H:|[chatBubbleView]-(64)-|", options: [], metrics: nil, views: views))
+            constraints.append(NSLayoutConstraint.constraints(withVisualFormat: "H:|[senderAvatar(25)][chatBubbleView]-(64)-|", options: [], metrics: nil, views: views))
         }
         constraints.flatMap { $0 }.activate()
     }
@@ -58,6 +69,10 @@ extension ChatCell: ViewModelConfigurable {
     
     func configure(with viewModel: ChatCellModel) {
         senderLabel.text = viewModel.sender.name
+        if viewModel.origin == .sent {
+            senderLabel.isHidden = true
+            senderAvatar.isHidden = true
+        }
         
         if let bubbleView = chatBubbleView {
             bubbleView.configure(with: viewModel.chatBubbleViewModel)
